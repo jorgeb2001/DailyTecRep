583d40bc8ca68e9034939822b0543160628b4234
jorgeb2001 <156154572+jorgeb2001@users.noreply.github.com>
2024-05-28 19:55:57 -0700

    Update eliminarEvento.php

--- eliminarEvento.php
+++ eliminarEvento.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Evento Eliminado</title>
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="description" name="Free HTML Templates">
-</head>
-<body>
-    <p>Redirigiendo a la página principal...</p>
-</body>
-</html>
-
 <?php
 include('auth.php');
 include('conexion.php');
