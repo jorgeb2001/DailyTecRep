0ee199153e56daed103690d84e42ad875ec7bc6e
jorgeb2001 <156154572+jorgeb2001@users.noreply.github.com>
2024-05-28 19:55:00 -0700

    Update authorE.php

--- authorE.php
+++ authorE.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Evento Eliminado</title>
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="description" name="Free HTML Templates">
-</head>
-<body>
-    <p>Redirigiendo a la página principal...</p>
-</body>
-</html>
-
 <?php
 session_start();  // Iniciar la sesión PHP
 
